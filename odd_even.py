7d5dae95a4526ada7548d79d25febd6b93603ec7
SnowyP <sklemon093@gmail.com>
2024-01-14 23:10:36 -0700

    Moved all files inside new folder for organization. All future single file Python scripts will go here

--- odd_even.py
+++
@@ -1,6 +0,0 @@
-# Checking a user input to see if the number they provided is odd or even
-number = int(input("Please enter your number to check if it's even or odd\n"))
-if number % 2 == 0: #Checks for remainder of the number. If a remainder exists after being divided by 2, it is an odd number
-    print(f"Your number {number} is even")
-else:
-    print(f"Your number {number} is odd")
