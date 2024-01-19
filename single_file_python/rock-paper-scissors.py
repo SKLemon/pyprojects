bc9fe06899037f943fcf22dace1a8afa70908e3a
SnowyP <sklemon093@gmail.com>
2024-01-19 14:21:56 -0700

    rock-paper-scissors.py file created. Going back with loops once finished

--- single_file_python/rock-paper-scissors.py
+++ single_file_python/rock-paper-scissors.py
@@ -30,28 +30,43 @@ scissors = '''
 ---.__(___)
 '''
 choices = [rock, paper, scissors] # Assigns the ASCII art above into table format for index call
+
 # Get User's input
 user_input = int(input("Choose Rock (0), Paper (1), or Scissors (2)\n")) #Converted to int as it will print a string
-print(f"You chose {user_input}")
 
-print(choices[user_input]) # Uses the int assigned as user_input to call the list index of the choices.
+
+# Ensuring user can only choose between 0, 1, or 2.
+if user_input >= 3 or user_input < 0:
+    print("Invalid choice. You lose!")
+    exit()
+    # print("Invalid Choice. Try again") # Once you know loops, go back and fix this line
+else:
+    choices_user = choices[user_input]
+    print(choices[user_input]) # Uses the int taken from user_input to call the list index of the choices... Because the user input happens to be the same as a list index.
 
 # Randomize computer input
-computer_input = random.randint(0,2) #This returns an integer
-print(f"Computer chose {computer_input}")
+computer_input = random.randint(0,2)
+print(f"Computer chose:")
 
-print(choices[computer_input]) # Same as above, but for the computer
+choices_computer = choices[computer_input]
+
+print(choices[computer_input]) # Same as in ln 37, but for the computer
 
 # Condition for winning or losing
-# if user_input == computer_input:
-#     print("It's a draw")
-# elif user_input < computer_input:
-#     print("You lose")
-# elif computer_input > user_input:
-#     print("You lose!")
-# elif user_input == 0 and computer_input == 2:
-#     print("You win!")
-# elif user_input > computer_input:
-#     print("You win!")
-# else:
-#     print("Invalid choice")
+# There's got to be a better solution for this... Too many if statements
+if choices_user == choices_computer:
+    print("It's a draw")
+elif choices_user == rock and choices_computer == scissors:
+    print("You win!")
+elif choices_user == rock and choices_computer == paper:
+    print("You lose!")
+elif choices_user == scissors and choices_computer == rock:
+    print("You lose!")
+elif choices_user == scissors and choices_computer == paper:
+    print("You win!")
+elif choices_user == paper and choices_computer == scissors:
+    print("You lose!")
+elif choices_user == paper and choices_computer == rock:
+    print("You win!")
+else:
+    print("Invalid Choice")
