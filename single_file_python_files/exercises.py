ffb5d8bd4afc393f87ce0c19c74144d4bfdc860c
SnowyP <sklemon093@gmail.com>
2024-07-02 23:29:35 -0600

    Long hiatus. Added day_5 loops and file name cleanup/organization

---
+++ single_file_python_files/exercises.py
@@ -0,0 +1,36 @@
+#Created as a part of the BAIL repository on Github. Created 01/19/2024 for additional practice and using a different source
+
+# Neat Arithmetic
+add = 5 + 3
+subtract = 5 - 3
+divide = (5/3)
+multiply = 5 * 3
+modulus = 5 % 3
+exponent = 5 ** 3
+floor_division = 5 // 3
+print(f"The result of the calculation 5 + 3 = {add}")
+print(f"The result of the calculation 5 - 3 = {subtract}")
+print(f"The result of the calculation 5 / 3 = {divide}")
+print(f"The result of the calculation 5 * 3 = {multiply}")
+print(f"The result of the calculation 5 to the power of 3 = {exponent}")
+
+##################################################################
+# Lists
+# Generally, since lists are a collection or group of objects, better to give them plural names
+# square brackets designate a list
+# dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+# dog = dogs[0]
+# print(dog.title()).title
+
+languages = ['python', 'C', 'java']
+print(f"A nice programming language is {languages[1]}.")
+#############################
+# Loops
+# Examining the for loop anatomy using a code block
+# for dog in dogs:
+    # print(dog)
+# Keyword "for" tells python to get a 'for loop' going
+# First keyword dog (with no s) is a temporary placeholder value. Python will place each item in the list into this placeholder, one at a time.
+# First time through the loop, it will output border collie#
+# Second time, it will output australian cattle dog ad so on.
+# Once there are no more items in the loop, the loop will end
