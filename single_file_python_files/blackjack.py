37d49797f8e685d5823a58615bdb99993e42cf92
SnowyP <sklemon093@gmail.com>
2024-12-05 00:56:53 -0700

    Added blackjack game. WIP on the limited # of cards

--- single_file_python_files/blackjack.py
+++ single_file_python_files/blackjack.py
@@ -1,7 +1,7 @@
 # Created as part of the BAIL repo on Github. Created 12/04/2024
 
 ### Blackjack House Rules ###
-# The deck is unlimited in size.
+# The deck is unlimited in size. -> #TO-DO 1: Change this to a deck size of 32 and remove a card each time the call_card function is called
 # There are no jokers.
 # The Jack/Queen/King all count as 10.
 # The Ace can count as 11 or 1.
@@ -9,16 +9,17 @@
 # cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 #
 # The cards in the list have equal probability of being drawn.
-# Cards are not removed from the deck as they are drawn.
+# Cards are not removed from the deck as they are drawn. #TO-DO 3: Related to TO-DO 1, removing the cards
 # The computer is the dealer.
 
+import time
 from art import logo_blackjack
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def call_card(cards, hand, num_cards=1):
-    """Adds a specific number of cards to the hand, from the cards list
+def call_card(cards, hand, score, num_cards=1):
+    """Adds a specific number of cards to the hand, from the cards list, and sums it up
     cards = cards list
     hand = The hand which recieves the card (dealers, yours etc)
     num_cards = number of cards you wish to draw, default is 1
@@ -26,10 +27,19 @@ def call_card(cards, hand, num_cards=1):
     for var in range (num_cards):
         card = random.choice(cards)
         hand.append(card)
-# Ask user to play - Possibly a while loop continously asking until no
 
-def blackjack():
-    user_win = True
+    score = sum(hand)
+
+    if 11 in hand and score > 21: # Replaces the Ace (value of 11) with an Ace (value of 1) to avoid going over 21, for both dealer and user
+        hand.remove(11)
+        hand.append(1)
+        score = sum(hand)
+
+    return score
+
+user_win = True
+while user_win == True:
+
     user_plays = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
 
     if user_plays == "y":
@@ -37,44 +47,51 @@ def blackjack():
         dealer_hand = []
         your_score = 0
         dealer_score = 0
+        your_score = call_card(cards,your_hand,your_score,2)
+        dealer_score = call_card(cards,dealer_hand,dealer_score,2)
         
-        call_card(cards,your_hand,2)
-        call_card(cards,dealer_hand,2)
-    else:
-        print("Goodbye")
-        user_plays = "n"
-
-    while user_plays == "y" and user_win == True:
         print("\n" * 20)
         print(logo_blackjack)
 
-        for card in your_hand:
-            your_score += card
-        for card in dealer_hand:
-            dealer_score += card
-
-        if your_score > 21:
-            print(f"Bust! Your score is: {your_score}")
-            user_win = False
-            blackjack()
-        if your_score == 21:
-            print("You win!")
-            blackjack()
-
-        print(f"Your cards: {your_hand}, current score: {your_score}")
-        print(f"Computer's first card: {dealer_hand[0]}")
-
-        user_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
-
-        if user_continue == "y":
-            call_card(cards,your_hand)
-            call_card(cards,dealer_hand)
+        while True:
             if your_score > 21:
-                print(f"Bust! Your score is: {your_score}")
-                user_win = False
-                blackjack()
-        else:
-            if dealer_score < 21:
-                call_card(cards,dealer_hand)
+                print(f"Bust! Your score is: {your_score}\n The dealer will now shuffle again")
+                time.sleep(3)
+                break
 
-blackjack()
+            elif your_score == 21:
+                print(f"You've hit blackjack with a score of {your_score}!\n The dealer will now shuffle again")
+                time.sleep(3)
+                break
+
+            elif dealer_score > 21:
+                print(f"Dealer went bust! Their score is: {dealer_score}\n The dealer will now shuffle again")
+                break
+
+            else:
+                print(f"Your cards: {your_hand}, current score: {your_score}")
+                print(f"Computer's first card: {dealer_hand[0]}")
+
+                user_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+
+                if user_continue == "y":
+                    your_score = call_card(cards,your_hand,your_score)
+                
+                elif user_continue == "n":
+                    while dealer_score <= 18:
+                        dealer_score = call_card(cards,dealer_hand,dealer_score)
+
+                    if dealer_score > 21:
+                        print("Dealer drew a card")
+                        print(f"Dealer went bust! Their score is: {dealer_score}\n The dealer will now shuffle again")
+
+                    elif your_score > dealer_score:
+                        print(f"You win! \n Your score: {your_score} \n Dealer's Score: {dealer_score}\n Dealer will now shuffle again")
+                        time.sleep(3)
+                    elif dealer_score > your_score:
+                        print(f"You lose! \n Your score: {your_score} \n Dealer's Score: {dealer_score}\n Dealer will now shuffle again")
+                        time.sleep(3)
+                    break
+    else:
+        user_win = False
+        print("Goodbye!")
