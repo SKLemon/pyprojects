4e42b085a93f7dda1b19552f49e7839aaf81cddc
SnowyP <sklemon093@gmail.com>
2024-12-08 22:46:07 -0700

    Started development of coffee machine code. Updated game data

---
+++ single_file_python_files/Coffee Machine/coffee_machine_code.py
@@ -0,0 +1,14 @@
+from game_data import MENU, resources
+
+print(MENU)
+print (resources)
+
+
+# TODO: 1) Print a report:
+# 1a) Database of sorts to keep tracking current resouces. Possibly a dictionary?
+# TODO: 2) Check resources are sufficient
+# 2a) Compares against how much has been used/how much is remaining, checks against the recipe of the drink
+# TODO: 3) Process coins
+# 3a) Coin operated only. Checks how much money was put in using amount of quarters, nickels, dimes, and pennies
+# TODO: 4) Check transaction successful
+# 5) Make coffee, and keep track of amount of money made
