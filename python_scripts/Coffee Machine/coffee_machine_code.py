702517d5168c7f432e1048f833fbdc7dcd7764ee
SnowyP <sklemon093@gmail.com>
2024-12-10 20:52:10 -0700

    Updated folder structure. Some comments written for clarity in coffee machine

--- python_scripts/Coffee Machine/coffee_machine_code.py
+++ python_scripts/Coffee Machine/coffee_machine_code.py
@@ -1,11 +1,4 @@
-import time
 from data import MENU, resources
-
-# COMPLETED TODO's:
-# TODO : 1) Print a report
-# TODO : 2) Check resources are sufficient
-# TODO : 3) Process coins
-# TODO : 4) Check transaction successful
 
 def format_report(resources):
     """Prints the formatted report.
@@ -13,11 +6,12 @@ def format_report(resources):
     Args:
         resources (dict): values of 'water', 'coffee', 'milk'
     """
-    for ingredient, quantity in resources.items():
+    for ingredient, quantity in resources.items(): # Extracts the key, value pair from a dict
         if ingredient in ['water','milk']:
             unit = "ml"
         else:
             unit = "g"
+
         print(f"{ingredient.title()}: {quantity}{unit}")
 
 def resource_check(resources,required_ingredients):
@@ -57,6 +51,7 @@ def coins(cash_in):
         accumulated_money (float): Returns the running total amount of money th_description_
     """
     total_coin_input = 0
+    print("Please insert coins.")
 
     for currency,value in cash_in.items():
         user_cash = int(input(f"How many {currency}?: "))
@@ -76,8 +71,9 @@ cash_in = {"quarters": 0.25,
             }
 
 while machine_on == True:
-
+    #Awaits user input, 2 hidden functions ('off', and 'report')
     user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
     if user_input in ["espresso", "latte", "cappuccino"]:
 
         required_ingredients = MENU[user_input]["ingredients"]
@@ -85,27 +81,23 @@ while machine_on == True:
 
         if enough_ingredients == True:
 
-            print("Please insert coins.")
-
-            accumulated_money = float(coins(cash_in))
-
+            accumulated_money = float(coins(cash_in)) # Asks for coin input only if there are enough ingredients
             required_money = MENU[user_input]["cost"]
 
             if accumulated_money < required_money:
                 print("Sorry, that's not enough money. Money refunded.")
 
             elif accumulated_money >= required_money:
+
                 change = accumulated_money - required_money
-
                 print(f"Here is ${change:.2f} in change")
-
                 profit += required_money
-
                 resource_removal(resources,required_ingredients)
 
     elif user_input == "off":
         machine_on = False
 
     elif user_input == "report":
+
         format_report(resources)
         print(f"Money: ${profit}")
