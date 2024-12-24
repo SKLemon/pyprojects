7a3463aa6236723148bb3dbe4efb4f852f97c88b
SnowyP <sklemon093@gmail.com>
2024-12-23 17:12:18 -0700

    Pong started

---
+++ python_oop/Instructions/Pong/main.py
@@ -0,0 +1,21 @@
+# Created for the BAIL repository on Github. Created 12/23/2024
+
+# TODO: 1) Set up the screen
+#           -
+# TODO: 2) Create the paddles
+#           - Create a second paddle for a 2-player game if need be (Ask the player how many players?)
+#           -
+# TODO: 3) Create the ball and make it move constantly
+#           - Detect collisions with wall and when it should bounce
+#           - Detect when misses occur
+# TODO: 4) Create the scoreboard
+#           - Track when misses occur and increase accordingly
+#           - Game over when reaching a certain number?
+
+"""
+Pong Game Implementation using Python's Turtle Graphics Module
+Players score points when the opponent misses the ball.
+Paddles are tracked using keyboard keys (up or down)
+Will need the screen set up, the ball and paddle physics, scoreboard to be calculated and added when scored on
+
+"""
---
+++ python_oop/Instructions/Pong/paddle.py
@@ -0,0 +1,1 @@
+""" This file contains the code for the paddle class. All attributes and methods, including the physics, the keys and button presses will be contained here """
