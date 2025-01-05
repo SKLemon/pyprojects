15cdbd760ac8db3185ce4e3625102d98ab6aa639
SnowyP <sklemon093@gmail.com>
2025-01-05 15:34:17 -0700

    Installed pandas, and updated CSV data instructional

--- python_oop/Instructions/Pandas Module/CSV Data/main.py
+++ python_oop/Instructions/Pandas Module/CSV Data/main.py
@@ -47,4 +47,21 @@ data_dict = (
     data_set.to_dict()
 )  # This converts the DataFrame object into a dictionary and places it into a variable
 
-temp_list = data_set["temp"].to_list() # --> This converts the "temperature" Series into a list and places it into a variable
+temp_list = data_set[
+    "temp"
+].to_list()  # --> This converts the "temperature" Series into a list and places it into a variable
+
+## Challenge is to get the average of all the temperatures in the weather data
+
+average = round(sum(temp_list) / len(temp_list), 2)
+print(f"The average is: {average}")  # This will print 17.43
+
+## The above is using standard math and python code. Pandas has a built in method to get the mean of the Series. Ex. shown below:
+
+
+# Important thing to note here is to take the series directly from the pd.read() rather than taking the converted variable on line 50
+print(f"The average using Series methods is: {round(data_set['temp'].mean(),2)}")
+
+## Challenge is to get the maximum value using the Series methods.
+
+print(f"The max value using the Series method is: {data_set['temp'].max()}")
