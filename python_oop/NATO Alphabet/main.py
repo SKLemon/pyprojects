e1a28addaa2f17b74a7463f168f68765870686ab
SnowyP <sklemon093@gmail.com>
2025-01-12 02:42:30 -0700

    Started NATO alphabet

---
+++ python_oop/NATO Alphabet/main.py
@@ -0,0 +1,11 @@
+# Created for BAIL repository on 01/12
+"""
+NATO Alphabet script. To take any word the user types and outputs its NATO phonetic equivalent. For example:
+VS Code = Victor, Sierra, Charlie, Oscar, Delta, Romeo
+"""
+
+# TODO: Create Dict in this format: {A:"Alpha", B: "Bravo"}.
+# Convert CSV into a dictionary where the keys are letters and values are corresponding code words for those letters
+
+# TODO: Create a list of phonetic code words input by the user.
+# I.E, User inputs "Hi", program spits out a list with ["Hotel", "India"]
