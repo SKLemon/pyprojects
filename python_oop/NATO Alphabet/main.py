17d88bd72152fa0ff05f635a377cccea0f930fc3
SnowyP <sklemon093@gmail.com>
2025-01-12 23:41:38 -0700

    NATO_Alphabet completed

--- python_oop/NATO Alphabet/main.py
+++ python_oop/NATO Alphabet/main.py
@@ -1,11 +1,32 @@
 # Created for BAIL repository on 01/12
+import pandas as pd
+from art import art
+
 """
-NATO Alphabet script. To take any word the user types and outputs its NATO phonetic equivalent. For example:
-VS Code = Victor, Sierra, Charlie, Oscar, Delta, Romeo
+NATO Alphabet Converter script:
+To take any word the user types and outputs its NATO phonetic equivalent.
+For Example:
+User types:
+VS Code
+
+Program shows:
+Victor, Sierra, Charlie, Oscar, Delta, Romeo
 """
+print(art)
 
 # TODO: Create Dict in this format: {A:"Alpha", B: "Bravo"}.
 # Convert CSV into a dictionary where the keys are letters and values are corresponding code words for those letters
 
+nato_data = pd.read_csv("./nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
+
 # TODO: Create a list of phonetic code words input by the user.
 # I.E, User inputs "Hi", program spits out a list with ["Hotel", "India"]
+
+while True:
+    user_input = input("What is your input?: ").upper()
+    converted_word = [phonetic_dict[letter] for letter in user_input]
+    print(converted_word)
+    user_continue = input("More words? (Y/N)").lower()
+    if user_continue == "n":
+        break
