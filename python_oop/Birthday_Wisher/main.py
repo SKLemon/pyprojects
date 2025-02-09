128b5c0a721e482613882f2fc5721f0785c0c442
SnowyP <sklemon093@gmail.com>
2025-02-09 01:51:11 -0700

    Initial commit

---
+++ python_oop/Birthday_Wisher/main.py
@@ -0,0 +1,23 @@
+# Created as a part of BAIL 02/09
+# ! Morality Clause for the Automated Birthday Wisher
+
+"""
+This automated birthday wisher is meant to remind you of birthdays and catch those "Oh no!" moments before they happen.
+It's not here to replace human connections and a program emailing "Happy Birthday!" isn't quite the same as calling your loved one...
+Additionally, who emails "Happy Birthday" messages nowadays...
+
+If you believe birthdays should always be remembered the old-fashioned way, feel free to pick up the phone and dial away!
+This program just ensures you never get hit with the "Oh no, I forgot!" panic again.
+Use responsibly, and when in doubt, add a personal touch — because even a robot knows that friendships shouldn't be fully automated.
+"""
+
+# TODO []: Remember to get your .env file set up asap, as well as any logging functionality if needed
+# TODO []: Birthdays.csv file needs creating/updated
+# TODO []: Checking if today matches a date in the birthdays.csv
+# TODO []: If today's date matches a date in the csv file, then pick a random letter from the templates
+# TODO []: Change any and all placeholders in the email with actual data from csv file
+# TODO []: Send email
+# TODO []:
+
+# Imports
+import pandas as pd
